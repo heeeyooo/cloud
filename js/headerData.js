57ada0c1025a139a2db1e8319c3e1dd399bfe45c
Stepan Dordiai <stepandordiai@gmail.com>
2024-12-11 20:45:48 +0200

    v3.3.1 Updated date format

--- js/headerData.js
+++ js/headerData.js
@@ -2,6 +2,6 @@ import dayjs from "https://unpkg.com/dayjs@1.11.13/esm/index.js";
 
 const today = dayjs();
 
-const formatDate = today.format("DD/MM/YY");
+const formatDate = today.format("D/MM/YY");
 
 document.querySelector(".header__day").innerHTML = formatDate;
